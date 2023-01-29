736dd2db4d6e9becba7d3933ab91be58469b0b38
BandServices <120313294+BandServices@users.noreply.github.com>
2023-01-29 10:50:55 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,58 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('https://bandservices.github.io/service-worker.js').then(function(registration) {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(err) {
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
+// Должно быть true в production
+var doCache = true;
+
+// Имя кэша
+var CACHE_NAME = 'my-pwa-cache-v2';
+
+// Очищает старый кэш
+self.addEventListener('activate', event => {
+   const cacheWhitelist = [CACHE_NAME];
+   event.waitUntil(
+       caches.keys()
+           .then(keyList =>
+               Promise.all(keyList.map(key => {
+                   if (!cacheWhitelist.includes(key)) {
+                       console.log('Deleting cache: ' + key)
+                       return caches.delete(key);
+                   }
+               }))
+           )
+   );
+});
+
+// 'install' вызывается, как только пользователь впервые открывает PWA 
+self.addEventListener('install', function(event) {
+   if (doCache) {
+       event.waitUntil(
+           caches.open(CACHE_NAME)
+               .then(function(cache) {
+                   // Получаем данные из манифеста (они кэшируются)
+                   fetch('/manifest.json')
+                       .then(response => {
+                           response.json()
+                       })
+                       .then(assets => {
+                       // Открываем и кэшируем нужные страницы и файлы
+                           const urlsToCache = [
+                               '/app/',
+                ........
+                               '/static/core/logo.svg*',
+                           ]
+                           cache.addAll(urlsToCache)
+                           console.log('cached');
+                       })
+               })
+       );
+   }
+});
+
+// Когда приложение запущено, сервис-воркер перехватывает запросы и отвечает на них данными из кэша, если они есть
+self.addEventListener('fetch', function(event) {
+   if (doCache) {
+       event.respondWith(
+           caches.match(event.request).then(function(response) {
+               return response || fetch(event.request);
+           })
+       );
+   }
+});
